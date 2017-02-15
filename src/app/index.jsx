571c959c622558deee688c3a3554d25973931a48
Joey Chagnon <chagnon.joey@gmail.com>
2017-02-14 22:03:10 -0500

    Add Material-UI scaffolding

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
-}
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
-render(<App/>, document.getElementById('app'));
+const App = () => (
+  <MuiThemeProvider>
+    <MyAwesomeReactComponent />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
