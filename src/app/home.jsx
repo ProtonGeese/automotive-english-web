2b85543561c295d574977398102b2fa80914b68f
Joey Chagnon <chagnon.joey@gmail.com>
2017-03-10 16:49:06 -0500

    Add icons to home page

--- src/app/home.jsx
+++ src/app/home.jsx
@@ -3,6 +3,13 @@ import { Link } from 'react-router';
 import {GridList, GridTile} from 'material-ui/GridList';
 import FlatButton from 'material-ui/FlatButton';
 
+import ActionFace from 'material-ui/svg-icons/action/face';
+import ActionAssignment from 'material-ui/svg-icons/action/assignment';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionHelp from 'material-ui/svg-icons/action/help';
+import ActionBook from 'material-ui/svg-icons/action/book';
+
 class HondaHome extends React.Component {
 
   static root_style = {
@@ -17,31 +24,52 @@ class HondaHome extends React.Component {
     overflowY: 'auto'
   }
 
+  static icon_style = {
+    width: '100%',
+    height: '100%'
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       gridData: [
         {
-          img: 'images/students.png',
+          icon: (
+            <ActionFace
+              style={HondaHome.icon_style}
+            />
+          ),
           title: 'Students',
           description: 'View and manage students',
           link: '/students'
         },
         {
-          img: 'images/lessons.png',
+          icon: (
+            <ActionAssignment
+              style={HondaHome.icon_style}
+            />
+          ),
           title: 'Lessons',
           description: 'View and manage lessons',
           link: '/lessons'
         },
         {
-          img: 'images/about.png',
+          icon: (
+            <ActionFavorite
+              style={HondaHome.icon_style}
+            />
+          ),
           title: 'About',
           description: 'Learn more about the AEP system',
           link: '/about'
         },
         {
-          img: 'images/help.png',
-          title: 'Help',
+          icon: (
+            <ActionBook
+              style={HondaHome.icon_style}
+            />
+          ),
+          title: 'Documentation',
           description: 'Learn to use the system',
           link: '/help'
         }
@@ -64,8 +92,9 @@ class HondaHome extends React.Component {
             key={e.img}
             title={e.title}
             subtitle={e.description}
+            titleBackground='rgba(36, 36, 36, 1)'
           >
-            <img src={e.img} />
+            {e.icon}
           </GridTile>
         ))}
         </GridList>
